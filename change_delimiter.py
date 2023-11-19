6708ca0652ea8c8629287e7a8c9c42e0979ffb60
elenadetoledo <elenadetoledohdez@gmail.com>
2023-11-19 10:36:02 +0100

    más avance

--- change_delimiter.py
+++
@@ -1,8 +0,0 @@
-#Change delimiter 
-import pandas as pd
-input_delimiter = ','
-output_delimiter = ';'
-
-
-df = pd.read_csv('listings.csv',delimiter=input_delimiter)
-df.to_csv('listings_changed.csv',sep=output_delimiter,index=False)
