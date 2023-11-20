3579a935a3e184ba5695a16171d5b37dbbe81e85
elenadetoledo <elenadetoledohdez@gmail.com>
2023-11-20 22:29:18 +0100

    desarrollo en parte entities

--- nlp.py
+++ nlp.py
@@ -5,15 +5,15 @@ import fitz
 
 nlp = spacy.load("en_core_web_sm")
 
-def	find_high_risk_countries(text, high_risk_countries):
+""" def	find_high_risk_countries(text, high_risk_countries):
 	matches = []
 	for country in high_risk_countries:
 		pattern = re.compile(r'\b{}\b'.format(re.escape(country.lower())))
 		if re.search(pattern, text):
 			matches.append(country)
-	return matches
+	return matches """
 
-def remove_non_printable(doc):
+def remove_non_printable(doc): #ver como usar esto, pero voy a tener que usarlo
     filtered_tokens = [token.text for token in doc if re.match(r'^[ -~]+$', token.text)]
     return spacy.tokens.Doc(doc.vocab, words=filtered_tokens)
 
@@ -28,18 +28,23 @@ def context_aware_country_search(text, high_risk_countries):
 
     return matches
 
-""" def context_aware_country_search_prueba(text, high_risk_countries):
+def is_part_of_larger_entity(entity, root, dep):
+    print(entity)
+    print(root)
+    return dep not in ("punct", "conj") and entity != root
+
+def context_aware_country_search_prueba2(text, high_risk_countries): #añadir q la tag sea GPE
     doc = nlp(text)
-    nlp.add_pipe('remove_non_printable',name="remove_non_printable",last = True) # TODO: BORRAR
     matches = []
 
-    for sent in doc.sents:
-        for ent in sent.ents:
-            if ent.text.lower() in high_risk_countries:
-                matches.append(sent.text.strip())
-                break  # Stop searching within the sentence if a match is found
+    for ent in doc.ents:
+        if ent.text.lower() in high_risk_countries:
+            print(ent.text, ent.label_)
+            print(ent.root.dep_)
+            if is_part_of_larger_entity(ent.text, ent.root, ent.root.dep_):
+                matches.append(ent.text.lower())
 
-    return matches """
+    return matches
 
 def read_pdf_and_search(file_path, high_risk_countries):
     # Open the PDF file
@@ -50,11 +55,12 @@ def read_pdf_and_search(file_path, high_risk_countries):
         for page_num in range(pdf_document.page_count):
             page = pdf_document[page_num]
             text += page.get_text()
-            #print(text)
+    
+    text = "I stayed at Avenue of Belgium and enjoyed the food at Restaurant Spain. Maldives is a beautiful country."
     preprocessed_text = text.lower()
 
     # Perform country search
-    matches = context_aware_country_search(preprocessed_text, high_risk_countries)
+    matches = context_aware_country_search_prueba2(preprocessed_text, high_risk_countries)
 
     # Output the matches
     if matches:
@@ -64,9 +70,9 @@ def read_pdf_and_search(file_path, high_risk_countries):
 
 # Example usage
 #pdf_file_path = "C:\\Desktop\\integrated-managemet-report-2022.pdf" Esto lo quiero cambiar!!!
-#pdf = "integrated-management-report-2022.pdf"
-pdf = "pruebas_SCIB.pdf"
-high_risk_countries = ["cuba","spain"] #tengo que controlar las distintas formas en las que se escriben ciertos paises!! EEUU y en varios idiomas
+pdf = "integrated-management-report-2022.pdf"
+#pdf = "pruebas_SCIB.pdf"
+high_risk_countries = ["cuba","spain","toronto", "belgium", "maldives"] #tengo que controlar las distintas formas en las que se escriben ciertos paises!! EEUU y en varios idiomas
 
 read_pdf_and_search(pdf, high_risk_countries)
 print("Fin del programa")
--- probando.py
+++ probando.py
@@ -1,10 +1,10 @@
-import spacy
+""" import spacy
 
 # Load the spaCy English model
 nlp = spacy.load("en_core_web_sm")
 
 # Sample text
-text = "I stayed at Hotel Maldives and enjoyed the food at Restaurant Cuba. Maldives is a beautiful country."
+text = "I stayed at Hotel Cuba and enjoyed the food at Restaurant España. Maldives is a beautiful country."
 #si pongo Hotel Cuba, lo reconoce como pais :(
 
 
@@ -27,4 +27,43 @@ final_locations = all_locations - filtered_locations
 # Print the final set of locations
 print("Final locations:")
 for loc in final_locations:
-    print(loc)
+    print(loc) """
+
+import spacy
+
+# Check if an entity is part of a larger entity
+def is_not_part_of_larger_entity(entity, root, dep):
+    return not(dep not in ("punct", "conj") and entity != root)
+
+# Load the spaCy English model
+nlp = spacy.load("en_core_web_sm")
+
+# Sample text
+text = "I stayed at Hotel Britain and enjoyed the food at Restaurant of Prague in Southern Italy. My beautiful house in the North of Spain is a beautiful country."
+matches = []
+# Process the text with spaCy
+doc = nlp(text)
+lista = ["belgium","spain","maldives","croatia", "prague", "cuba","france"]
+
+for ent in doc.ents:
+    print(ent)
+for ent in doc.ents:
+    print(f"Entity: {ent.text}, Root: {ent.root.text}, Dependency: {ent.root.dep_}")
+    if ent.text.lower() in lista:
+        if is_not_part_of_larger_entity(ent.text, ent.root.text , ent.root.dep_):
+            matches.append(ent.text.lower())
+print(matches)
+        
+""" # Extract named entities and their syntactic dependencies
+entity_relations = [(ent.text, ent.root.text, ent.root.dep_) for ent in doc.ents]
+
+# Print the named entities and their syntactic dependencies
+for entity, root, dep in entity_relations:
+    print(f"Entity: {entity}, Root: {root}, Dependency: {dep}") """
+
+
+""" 
+# Identify entities that are part of a larger entity
+for entity, root, dep in entity_relations:
+    if is_part_of_larger_entity(entity, root, dep):
+        print(f"{entity} is part of a larger entity.") """
